96006c8d6f1bf43546808eccaada88f85ba35eb6
Helen Glushakova <hellgenxd@gmail.com>
2025-10-28 22:00:14 +0700

    Initialize project

---
+++ src/main/java/com/helen/dnd_charachter_editor/DndCharachterEditorApplication.java
@@ -0,0 +1,13 @@
+package com.helen.dnd_charachter_editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndCharachterEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndCharachterEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helen/dnd_charachter_editor/DndCharachterEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helen.dnd_charachter_editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndCharachterEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
