b34df3654c9432bf4171ac0b6d48a7b2be1a6ed2
gazco <gazcoin-1@bk.ru>
2025-11-22 00:46:58 +0700

    DND: чиним деплой 3

--- src/test/java/com/helen/dnd_charachter_editor/DndCharachterEditorApplicationTests.java
+++ src/test/java/com/helen/dnd_charachter_editor/DndCharachterEditorApplicationTests.java
@@ -3,11 +3,14 @@ package com.helen.dnd_charachter_editor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class DndCharachterEditorApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
